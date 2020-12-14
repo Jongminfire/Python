54aaa36c4c2cc76f7263b1d8ef8c52b0879b0446
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-12-14 16:31:13 +0900

    모든 경우를 조합해서 최댓값 찾기

---
+++ Algorithm PS/Baekjoon/2217 (로프, 그리디).py
@@ -0,0 +1,15 @@
+n = int(input())
+rope = []
+ans = 0
+
+for i in range (n):
+  rope.append(int(input()))
+
+rope.sort()
+
+for r in range (len(rope)):
+  ans = max(ans,rope[r]*(len(rope)-r))
+
+print(ans)
+
+# 모든 로프의 값을 계산해서 최대 값을 찾았음
