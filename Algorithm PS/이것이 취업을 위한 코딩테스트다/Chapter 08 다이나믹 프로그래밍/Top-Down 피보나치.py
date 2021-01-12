1b998e0ad24534fed67fde30515a92d02ce8a931
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-01-12 16:20:18 +0900

    피보나치 탑 다운 형식

---
+++ Algorithm PS/이것이 취업을 위한 코딩테스트다/Chapter 08 다이나믹 프로그래밍/Top-Down 피보나치.py
@@ -0,0 +1,13 @@
+d = [0] * 1000
+
+def fibo(x):
+  # 종료 조건 (1 혹은 2일 때 1을 반환)
+  if x == 1 or x == 2:
+    return 1
+  
+  # 이미 계산한 적 있는 문제라면 그대로 반환
+  if d[x] != 0:
+    return d[x]
+  
+  d[x] = fibo(x-1)+fibo(x-2)
+  return d[x]
