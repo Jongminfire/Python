56a53b947f94807c89fecaaa242eb2b08de992f9
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-04-06 14:44:42 +0900

    2차원 그래프에서의 분할정복
    쿼드트리, 색종이 만들기 유형의 문제

---
+++ 알고리즘 정리/분할 정복/2차원 그래프에서의 분할정복.py
@@ -0,0 +1,32 @@
+n = int(input())
+board = [list(map(int,input().split())) for _ in range(n)]
+white = 0
+blue = 0
+
+def divide (x,y,size):
+  global blue
+  global white
+  color = board[x][y]
+
+  for nx in range(x,x+size):
+    for ny in range(y,y+size):
+      if color != board[nx][ny]:
+        divide(x,y,size//2)
+        divide(x+size//2,y,size//2)
+        divide(x,y+size//2,size//2)
+        divide(x+size//2,y+size//2,size//2)
+
+        return
+  
+  if color == 1:
+    blue += 1
+  else:
+    white += 1
+
+divide(0,0,n)
+
+print(white)
+print(blue)
+
+# https://www.acmicpc.net/submit/2630/28083282 (백준 2630 색종이만들기 문제)
+# 왼쪽 위, 오른쪽 위,왼쪽 아래,오른쪽 아래의 네 부분으로 나누어서 요소의 개수 세기
